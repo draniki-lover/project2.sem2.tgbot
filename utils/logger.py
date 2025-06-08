908842cb3843b10530a54859e30b8587e2759a83
draniki-lover <kshmankova@gmail.com>
2025-06-08 17:33:04 +0300

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,23 +1,32 @@
 import logging
+from pathlib import Path
 
-def get_logger():
-    logger = logging.getLogger()
+
+def get_logger(name: str = "bot"):
+    log_dir = Path("logs")
+    log_dir.mkdir(exist_ok=True)
+
+    logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
 
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.INFO)
-    console_fmt = logging.Formatter(
-        "[%(asctime)s] %(levelname)s in %(module)s: %(message)s",
-        "%Y-%m-%d %H:%M:%S"
+    log_format = logging.Formatter(
+        "[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
     )
-    console_handler.setFormatter(console_fmt)
+
+    file_handler = logging.FileHandler(
+        filename=log_dir / "main.log",
+        encoding="utf-8",
+        mode="a"
+    )
+    file_handler.setFormatter(log_format)
+    file_handler.setLevel(logging.INFO)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(log_format)
+    console_handler.setLevel(logging.INFO)
+
+    logger.addHandler(file_handler)
     logger.addHandler(console_handler)
 
-    file_handler = logging.FileHandler("bot.log", encoding="utf-8", mode="a")
-    file_handler.setLevel(logging.INFO)
-    file_fmt = logging.Formatter(
-        "[%(asctime)s] %(levelname)s in %(module)s: %(message)s",
-        "%Y-%m-%d %H:%M:%S"
-    )
-    file_handler.setFormatter(file_fmt)
-    logger.addHandler(file_handler)
+    return logger
