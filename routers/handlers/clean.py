66485b9f1367f3595618f1845f347d8a5eeb1713
draniki-lover <kshmankova@gmail.com>
2025-06-08 15:54:26 +0300

    Update clean.py

--- routers/handlers/clean.py
+++ routers/handlers/clean.py
@@ -11,12 +11,10 @@ router = Router()
 @router.message(Command("delete"))
 async def delete_history(message: types.Message):
     storage = GraphStorage()
-
-    # Очищаем файл с данными
+    
     with open(storage.data_file, 'w') as f:
         json.dump({}, f)
-
-    # Удаляем все файлы графиков из директории
+        
     for file in storage.storage_dir.glob("*.png"):
         try:
             file.unlink()
