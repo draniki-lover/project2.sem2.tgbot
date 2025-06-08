ccb41fd786ef576354d7d89060d8e14f9da913be
draniki-lover <kshmankova@gmail.com>
2025-06-08 15:23:02 +0300

    Add files via upload

--- routers/commands.py
+++ routers/commands.py
@@ -1,1 +1,26 @@
+from aiogram.filters import CommandStart, Command
+from aiogram.fsm.context import FSMContext
+from aiogram import types, Router
 
+from keyboards import inlines
+from states.state import Form
+
+router = Router()
+
+@router.message(CommandStart())
+async def start(message: types.Message, state: FSMContext) -> None:
+    await state.set_state(Form.waiting_communication_style)
+    await message.answer(f"{message.chat.full_name}, привет👋!\n"
+                         "Это бот, который поможет тебе грамотно строить графики🌝\n"
+                         "Нажми /help, если хочешь ближе ознакомиться с возможностями бота.\n"
+                         "Что называется, let's try!!!",
+                         reply_markup=inlines.start_kb())
+
+@router.message(Command("help"))
+async def help(message: types.Message):
+    await message.answer(f"{message.chat.full_name}, вот список команд, реализованных в этом боте:\n"
+                         "- /about, чтобы узнать подробнее о проекте: сути, его полезности✍️,\n"
+                         "- /start, чтобы начать коммуникацию с ботом заново🙌,\n"
+                         "- /history, чтобы увидеть, какие графики были уже построены📐,\n"
+                         "- /delete, чтобы очистить историю запущенных графиков."
+                         )
